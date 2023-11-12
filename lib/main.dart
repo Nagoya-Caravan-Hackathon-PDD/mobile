c996776b207de2817197834ed9cd92f7c20e93b9
yudai2929 <yudai201729@icloud.com>
2023-11-12 16:30:56 +0900

    👍fix: titleを変更

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Github Monster',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: ThemeColor.secondary),
         useMaterial3: true,
