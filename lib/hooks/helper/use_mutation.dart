086c38d1cf08070690b645653e5f0728503ef5fb
yudai2929 <yudai201729@icloud.com>
2023-11-12 22:18:56 +0900

    👍feat: use_mutationを追加

---
+++ lib/hooks/helper/use_mutation.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+// NOTE: mutateFnの戻り値の型と、mutateの引数の型をgenericにする
+class UseMutationResult<T, P> {
+  final T? data;
+  final bool isLoading;
+  final Error? error;
+  final Future<void> Function({P params, MutationOption<T>? option}) mutate;
+  final bool hasError;
+
+  UseMutationResult({
+    required this.data,
+    required this.isLoading,
+    required this.error,
+    required this.mutate,
+    required this.hasError,
+  });
+}
+
+typedef MutationFunction<T, P> = Future<T> Function(P);
+
+class MutationOption<T> {
+  final void Function(T) onSuccess;
+  final void Function(Error) onError;
+
+  MutationOption({
+    required this.onSuccess,
+    required this.onError,
+  });
+}
+
+UseMutationResult<T, P> useMutation<T, P>({
+  required MutationFunction<T, P> mutateFn,
+}) {
+  final data = useState<T?>(null);
+  final isLoading = useState(false);
+  final error = useState<Error?>(null);
+  final hasError = useState(false);
+
+  Future<void> mutate({params, MutationOption<T>? option}) async {
+    isLoading.value = true;
+    error.value = null;
+    hasError.value = false;
+    try {
+      final res = await mutateFn(params);
+      data.value = res;
+      if (option?.onSuccess != null) option!.onSuccess(res);
+    } on Error catch (e) {
+      error.value = e;
+      if (option?.onError != null) option!.onError(e);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  return UseMutationResult(
+      data: data.value,
+      isLoading: isLoading.value,
+      error: error.value,
+      mutate: mutate,
+      hasError: hasError.value);
+}
